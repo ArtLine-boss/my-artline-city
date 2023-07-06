58ee832c7ca59fa14230fc800d4c0eb49b547e04
Uladzislau Prylutski <wladfm.prylutski@gmail.com>
2023-07-06 22:58:31 +0300

    Добавление пункта меню Маше

--- pages/menu1.php
+++ pages/menu1.php
@@ -1,44 +1,44 @@
 <?
-	// Меню
-	
-	//определяем полное имя пользователя
-	$select = "SELECT USER_FIO,color_default FROM users WHERE USER_LOGIN='".$login."'";
-	$query = mysql_query($select) or die(null);
-	$username = "";
-	$color_default = "";
-	if($row = mysql_fetch_array($query)) {
-		if(!empty($row['USER_FIO'])) {
-			$username = $row['USER_FIO'];
-		}
-		if($row['color_default'] == 1) {
-			$color_default = " data-color-default='true'";
-			?>
-			<script>
-				var addNewYear = false;
-			</script>
-			<?php
-		} else {
-			?>
-			<script>
-				var addNewYear = true;
-			</script>
-			<?php
-		}
-	}
-	
-	$poruchnie = "";
-	if($admin  == 3 || $admin  == 4) {
-		$poruchnie = '<li><a href="#" onclick="ComboUser()"><i class="fa fa-user-circle fa-fw"></i> Совмещенный пользователь</a></li>
+// Меню
+
+//определяем полное имя пользователя
+$select = "SELECT USER_FIO,color_default FROM users WHERE USER_LOGIN='" . $login . "'";
+$query = mysql_query($select) or die(null);
+$username = "";
+$color_default = "";
+if ($row = mysql_fetch_array($query)) {
+    if (!empty($row['USER_FIO'])) {
+        $username = $row['USER_FIO'];
+    }
+    if ($row['color_default'] == 1) {
+        $color_default = " data-color-default='true'";
+        ?>
+        <script>
+            var addNewYear = false;
+        </script>
+        <?php
+    } else {
+        ?>
+        <script>
+            var addNewYear = true;
+        </script>
+        <?php
+    }
+}
+
+$poruchnie = "";
+if ($admin == 3 || $admin == 4) {
+    $poruchnie = '<li><a href="#" onclick="ComboUser()"><i class="fa fa-user-circle fa-fw"></i> Совмещенный пользователь</a></li>
 						<li class="divider"></li>';
-	}
-	
-	$fon = "";
-	if($admin == 4 || $admin  == 6 || $admin  == 7 || $admin == 8) {
-		$fon = '<li><a href="#" onclick="SelectedFont()"><i class="fa fa-tint fa-fw"></i> Изменить раскраску</a></li>
+}
+
+$fon = "";
+if ($admin == 4 || $admin == 6 || $admin == 7 || $admin == 8) {
+    $fon = '<li><a href="#" onclick="SelectedFont()"><i class="fa fa-tint fa-fw"></i> Изменить раскраску</a></li>
 						<li class="divider"></li>';
-	}
-	
-	echo '<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+}
+
+echo '<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
 	<div class="navbar-header"><img id="logoSystem" class="logo" src="../../dist/css/logo-white.png" alt="Logo">
 	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 	<span class="sr-only">Toggle navigation</span>
@@ -46,14 +46,14 @@
 	<span class="icon-bar"></span>
 	<span class="icon-bar"></span>
 	</button>
-	<a class="navbar-brand" href="../orders.php" style="color: #ffffff" id="headerSystem"'.$color_default.'>Система управления заказами</a>
+	<a class="navbar-brand" href="../orders.php" style="color: #ffffff" id="headerSystem"' . $color_default . '>Система управления заказами</a>
 	</div>
 	<ul class="nav navbar-top-links navbar-right">
 	<li class="dropdown">
 	<a class="menu_a" class="dropdown-toggle" data-toggle="dropdown" href="#">
-	<i class="fa fa-user fa-fw"></i> '.$username.' <i class="fa fa-caret-down"></i>
+	<i class="fa fa-user fa-fw"></i> ' . $username . ' <i class="fa fa-caret-down"></i>
 	</a>
-	<ul class="dropdown-menu dropdown-user">'.$poruchnie.$fon.'
+	<ul class="dropdown-menu dropdown-user">' . $poruchnie . $fon . '
 	<li><a href="../rpassword.php?current"><i class="fa fa-gear fa-fw"></i> Сбросить пароль</a></li>
 	<li class="divider"></li>
 	<li><a href="../exit.php"><i class="fa fa-sign-out fa-fw"></i> Выход</a></li>
@@ -61,70 +61,78 @@
 	</li>
 	</ul>
 	';
-	echo ' <div class="navbar-default sidebar" role="navigation">
+echo ' <div class="navbar-default sidebar" role="navigation">
 	<div class="sidebar-nav navbar-collapse">
 	<ul class="nav" id="side-menu">';
-	
-	
-	if ($admin == '4' OR $admin == '2' OR $admin == '3'){
-		echo '<li><a class="menu_a" href="../orders.php"><i class="fa fa-shopping-cart fa-fw"></i> Заказы</a></li>';
-		if(in_array(2, $ACCESS_ROLES)) {
-			echo '<li><a class="menu_a" href="/www/index.php?m=calculator"><i class="fa fa-calculator fa-fw"></i> Калькулятор</a></li>';
-		}
-		echo '<li><a class="menu_a" href="../clients.php"><i class="fa fa-align-justify fa-fw"></i> Клиенты</a></li>
+
+
+if ($admin == '4' or $admin == '2' or $admin == '3') {
+    echo '<li><a class="menu_a" href="../orders.php"><i class="fa fa-shopping-cart fa-fw"></i> Заказы</a></li>';
+    if (in_array(2, $ACCESS_ROLES)) {
+        echo '<li><a class="menu_a" href="/www/index.php?m=calculator"><i class="fa fa-calculator fa-fw"></i> Калькулятор</a></li>';
+    }
+    echo '<li><a class="menu_a" href="../clients.php"><i class="fa fa-align-justify fa-fw"></i> Клиенты</a></li>
 		<li><a class="menu_a" href="list_contact_fresh.php"><i class="fa fa-link fa-fw"></i> Неперенесенные контакты</a></li>';
-	}
-	if($admin == '4' || $login == '026' || $login == 'buh2') {
-		echo '<li><a class="menu_a" href="../artliner_report.php"><i class="fa fa-book fa-fw"></i> Отчет по артлайнерам</a></li>';
-	}
-	if ($admin == '4' OR $admin == '2' OR $login == '008' OR $login == '026' OR $login == '030' OR $login == '028' or $login == '033' or $login == '032'){
-		echo '<li><a class="menu_a" href="../tn_list.php"><i class="fa fa-money fa-fw"></i>ТН</a></li>';
-		echo ' <li><a class="menu_a" href="../oplati.php"><i class="fa fa-money fa-fw"></i> Оплаты</a></li>';
-	}
-	if($login == '026' OR  $admin == '030' or $login == '033' or $login == '032'){
-		echo '<li>
+}
+if ($admin == '4' || $login == '026' || $login == 'buh2') {
+    echo '<li><a class="menu_a" href="../artliner_report.php"><i class="fa fa-book fa-fw"></i> Отчет по артлайнерам</a></li>';
+}
+if ($admin == '4' or $admin == '2' or $login == '008' or $login == '026' or $login == '030' or $login == '028' or $login == '033' or $login == '032') {
+    echo '<li><a class="menu_a" href="../tn_list.php"><i class="fa fa-money fa-fw"></i>ТН</a></li>';
+    echo ' <li><a class="menu_a" href="../oplati.php"><i class="fa fa-money fa-fw"></i> Оплаты</a></li>';
+}
+if ($login == '026' or $admin == '030' or $login == '033' or $login == '032') {
+    echo '<li>
 		<a class="menu_a" href="../stock.php"><i class="fa fa-money fa-fw"></i> Склад</a>
-	</li>';}
-	if ($admin == '4' OR  $admin == '5' OR $admin == '6' OR  $admin == '7'){	echo ' <li><a class="menu_a" href="../task_p_d.php"><i class="fa fa-th-list fa-fw"></i>Подготовка файлов </a></li>';}
-	if($admin == '4' OR  $admin == '9'){
-		
-		echo '<li>
+	</li>';
+}
+if ($admin == '4' or $admin == '5' or $admin == '6' or $admin == '7') {
+    echo ' <li><a class="menu_a" href="../task_p_d.php"><i class="fa fa-th-list fa-fw"></i>Подготовка файлов </a></li>';
+}
+if ($admin == '4' or $admin == '9') {
+
+    echo '<li>
 		<a class="menu_a" href="../stock.php"><i class="fa fa-money fa-fw"></i> Склад</a>
 		</li>';
-	}	
-	if($admin == '9'){
-		echo '	<li><a class="menu_a" href="../klass.php">Классификатор</a></li>';
-	}
-	if($login == 'preprint'){
-		
-		echo '<li>
+}
+if ($admin == '9') {
+    echo '	<li><a class="menu_a" href="../klass.php">Классификатор</a></li>';
+}
+if ($login == 'preprint') {
+
+    echo '<li>
 		<a class="menu_a" href="../stamps.php"><i class="fa fa-money fa-fw"></i> Штампы</a>
 	</li>';
-	}
-	if($login == '15'){
-	
-	echo '<li>
+}
+if ($login == '15') {
+
+    echo '<li>
 	<a class="menu_a" href="diz.php"><i class="fa fa-money fa-fw"></i> Загруженность дизайнеров</a>
 	</li>';
-	}							
-	if($login == '8' || $login == '29' || $login == '030'){
-	
-	echo '<li>
+}
+if ($login == '8' || $login == '29' || $login == '030') {
+
+    echo '<li>
 	<a class="menu_a" href="mang_bank.php"> Оформленные заказы</a>
 	</li>';
-	}	
-	if($admin == '2'){
-	
-	echo '<li>
+}
+if ($admin == '2') {
+
+    echo '<li>
 	<a class="menu_a" href="mang_bank.php"> Оформленные заказы</a>
 	</li>';
-	}	
-	if ($admin == '4' OR $admin == '2' OR $admin == '3'){
-	echo '<li><a class="menu_a" href="../task_p_list.php"><i class="fa fa-th-list fa-fw"></i>Загруженность производства</a></li>';
-	}
-	if ($admin == '4'){
-	
-	echo '
+}
+if ($admin == '4' or $admin == '2' or $admin == '3') {
+    echo '<li><a class="menu_a" href="../task_p_list.php"><i class="fa fa-th-list fa-fw"></i>Загруженность производства</a></li>';
+}
+
+if ($login == '008') {
+    echo '<li><a class="menu_a" href="../task.php"><i class="fa fa-th-list fa-fw"></i>Производство</a></li>';
+}
+
+if ($admin == '4') {
+
+    echo '
 	<li><a class="menu_a" href="../task.php"><i class="fa fa-th-list fa-fw"></i>Производство</a></li>
 	<li><a class="menu_a" href="#" ><i class="fa fa-file-text-o fa-fw"></i> Отчет <span class="fa arrow"></span></a>
 	<ul class="nav nav-second-level">
@@ -162,14 +170,14 @@
 	</li>
 	</ul>
 	</li>';
-	}
-	echo '</ul>
+}
+echo '</ul>
 	</div>
 	</div>
 	</nav>';
 
 // Сообщения
 include_once($_SERVER['DOCUMENT_ROOT'] . '/www/popup/index.php');
-	
-	
-	?>	
+
+
+?>
